71e61aaaf085eb7687958d1f308251c6f0916f8c
Ali <ali@umaev.ru>
2020-10-23 18:19:04 +0300

    ДЗ 5 2.0

--- README.md
+++
@@ -1,1 +0,0 @@
-PythonGeekBrains
--- Task#5 18.15.07.py
+++ Task#5 18.15.07.py
@@ -3,14 +3,14 @@
 
 from random import randint
 
-txtFile = open("txtFileForTask#5.txt", "r+")
+txtFile = open("txtFileForTask#5.txt", "w")
 
 for i in range(randint(10, 100)):
     txtFile.write(f"{randint(10, 1000)} ")
+txtFile.close()
 
-fileInfo = txtFile.readline(0).split()
-print(fileInfo)
-
+txtFile = open("txtFileForTask#5.txt", "r")
+fileInfo = txtFile.readline().split()
+print(f"Информация из файла: {fileInfo}")
 print(f"Сумма всех чисел в файле: {sum(map(float, fileInfo))}")
-
 txtFile.close()
